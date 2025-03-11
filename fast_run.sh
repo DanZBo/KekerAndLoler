2a6656038240166e162ce1c25f1dc169562e200b
DanZBo <daniilbogocanov@Daniils-MacBook-Air.local>
2025-03-12 00:26:43 +0500

    bug fix

--- fast_run.sh
+++ fast_run.sh
@@ -1,17 +1,17 @@
 
-if [ -z "$SERVER_NAME" ] || [ -z "$PASSWORD" ]; then
-  echo "Error: SERVER_NAME or PASSWORD is not set!"
+if [ -z "$DOMAIN_NAME" ] || [ -z "$PASSWORD" ]; then
+  echo "Error: DOMAIN_NAME or PASSWORD is not set!"
   exit 1
 fi
 
 cp ./nginx/nginx_example.conf ./nginx/nginx.conf
-sed -i "s/{YOUR_SERVER_NAME}/$SERVER_NAME/g" ./nginx/nginx.conf
+sed -i "s/{YOUR_DOMAIN_NAME}/$DOMAIN_NAME/g" ./nginx/nginx.conf
 
 cp ./config_example.json ./config.json
 sed -i "s/{PASSWORD}/$PASSWORD/g" ./config.json
-sed -i "s/{YOUR_SERVER_NAME}/$SERVER_NAME/g" ./config.json
+sed -i "s/{YOUR_DOMAIN_NAME}/$DOMAIN_NAME/g" ./config.json
 
 docker-compose up -d --build
 
 echo "Done! Your connection url:
-trojan://$PASSWORD@$SERVER_NAME:443?security=tls&sni=$SERVER_NAME&type=tcp#Me"
+trojan://$PASSWORD@$DOMAIN_NAME:443?security=tls&sni=$DOMAIN_NAME&type=tcp#Me"
